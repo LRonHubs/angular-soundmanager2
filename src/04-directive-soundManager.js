be32d88b42659de9581942a5aa2d8c73d4c2b44f
Kamron Batman <kamronbatman@users.noreply.github.com>
2015-09-10 00:09:52 -0700

    [Fix] Fixes '$apply already in progress' bug
    Fixes '$apply already in progress' bug with all $on events.

--- src/04-directive-soundManager.js
+++ src/04-directive-soundManager.js
@@ -1,41 +1,41 @@
-ngSoundManager.directive('soundManager', ['$filter', 'angularPlayer',
-    function($filter, angularPlayer) {
+ngSoundManager.directive('soundManager', ['$filter', '$timeout', 'angularPlayer',
+    function($filter, $timeout, angularPlayer) {
         return {
             restrict: "E",
             link: function(scope, element, attrs) {
                 //init and load sound manager 2
                 angularPlayer.init();
                 scope.$on('track:progress', function(event, data) {
-                    scope.$apply(function() {
+                    $timeout(function() {
                         scope.progress = data;
-                    });
+                    }, 0);
                 });
                 scope.$on('track:id', function(event, data) {
-                    scope.$apply(function() {
+                    $timeout(function() {
                         scope.currentPlaying = angularPlayer.currentTrackData();
-                    });
+                    }, 0);
                 });
                 scope.$on('currentTrack:position', function(event, data) {
-                    scope.$apply(function() {
+                    $timeout(function() {
                         scope.currentPostion = $filter('humanTime')(data);
-                    });
+                    }, 0);
                 });
                 scope.$on('currentTrack:duration', function(event, data) {
-                    scope.$apply(function() {
+                    $timeout(function() {
                         scope.currentDuration = $filter('humanTime')(data);
-                    });
+                    }, 0);
                 });
                 scope.isPlaying = false;
                 scope.$on('music:isPlaying', function(event, data) {
-                    scope.$apply(function() {
+                    $timeout(function() {
                         scope.isPlaying = data;
-                    });
+                    }, 0);
                 });
                 scope.playlist = angularPlayer.getPlaylist(); //on load
                 scope.$on('player:playlist', function(event, data) {
-                    scope.$apply(function() {
+                    $timeout(function() {
                         scope.playlist = data;
-                    });
+                    }, 0);
                 });
             }
         };
